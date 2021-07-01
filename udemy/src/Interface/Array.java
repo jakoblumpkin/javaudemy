5cb215a6ef98ff35476cbf640be9a3cdb2b55028
jakob <jakob.lumpkin@ibm.com>
2021-07-01 17:42:54 -0500

    after array

--- udemy/src/Interface/Array.java
+++ udemy/src/Interface/Array.java
@@ -1,5 +1,8 @@
 package Interface;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 
 public class Array {
 
@@ -25,8 +28,61 @@ public class Array {
 		mergeList.addAll(list2);
 		mergeList.addAll(list3);
 		
-		System.out.println(mergeList.get(1));
+		//System.out.println(mergeList.get(1));
+		//System.out.println(mergeList.size());
 		
+		//for(int i = 0; i <= mergeList.size(); i++) {
+		//	System.out.println(mergeList.get(i));
+		//}
+		
+		List<String> words = List.of("Apple", "Bat", "Cat");
+		List<String> wordsAl = new ArrayList<>(words);
+		System.out.println(words);
+		System.out.println(wordsAl);
+		
+		/*for(String word: words) {
+			if(word.endsWith("at")) {
+				System.out.println(word);
+			}
+		}
+		
+		for(String word: wordsAl) {
+			if(word.endsWith("at")) {
+				wordsAl.remove(word);
+			}
+		}*/
+		
+		//Remove from list using iternator
+		
+		Iterator<String> iterator = wordsAl.iterator();
+		while(iterator.hasNext()) {
+			if(iterator.next().endsWith("at")) {
+				iterator.remove();
+			}
+		}
+		
+		System.out.println(wordsAl);
+		
+		List value = List.of("A", 'A', 1, 1.0);
+		
+		List<Integer> numbers = List.of(101, 102, 103, 104, 105);
+		
+		//stem.out.println(numbers.indexOf(101));
+		
+		List<Integer> numberAl = new ArrayList<>(numbers);
+		
+		numberAl.indexOf(101);
+		
+		numberAl.remove(Integer.valueOf(101));
+		
+		//System.out.println(numberAl);
+		
+		List<Integer> numbers1 = List.of(123, 12, 3, 45);
+		
+		List<Integer> numberAll = new ArrayList<>(numbers1);
+		
+		Collections.sort(numberAll);
+		System.out.println(numberAll);
 	}
 
 }

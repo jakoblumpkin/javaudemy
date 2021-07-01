c72ee50cee89577b61cdc39882ec465ca80eb602
jakob <jakob.lumpkin@ibm.com>
2021-07-01 08:55:03 -0500

    array

--- udemy/src/udemy/main.java
+++ udemy/src/udemy/main.java
@@ -11,6 +11,31 @@ public class main {
 		jakob.getMax();
 		jakob.getMin();
 		System.out.println(jakob.getAverage());
+		sum(9, 3, 4);
 	}
-
+	
+	//Add as any and outputs a array
+	public static void sum(int... value) {
+		String [] days = new String [7];
+		days[0] = "Sunday";
+		days[1] = "Monday";
+		days[2] = "Tuesday";
+		days[3] = "Wednesday";
+		days[4] = "Thursday";
+		days[5] = "Friday";
+		days[6] = "Saturday";
+		
+		String current = "Monday";
+		int Number = 0;
+		
+		for (int i = 0; i < days.length; i++) {
+			int length = days[i].length();
+			if( length  > Number ) {
+				Number = length;
+				current = days[i];
+			};
+		}
+		System.out.println(current);
+	}
+ 
 }
